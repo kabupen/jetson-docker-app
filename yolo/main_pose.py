ec8c004f4d38e399494e4f01cb039d62e00f071b
Kosuke Takeda <kosuke050205@gmail.com>
2024-10-28 16:08:54 +0900

    yolo posek

--- yolo/main_pose.py
+++ yolo/main_pose.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 import cv2
 from tqdm import tqdm
 from ultralytics import YOLO
@@ -31,22 +32,22 @@ keypoint_pairs = [
 
 if __name__ == "__main__" : 
 
-    source_video_path = "20241007.mov"
-    target_video_path = "output.mp4"
-    confidence_threshold = 0.3
-    iou_threshold = 0.7
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source_video_path", type=str, required=True)
+    parser.add_argument("-t", "--target_video_path", type=str, required=True)
+    parser.add_argument("-c", "--confidence_threshold", type=float, default=0.3)
+    parser.add_argument("-i", "--iou_threshold", type=float, default=0.7)
+    args = parser.parse_args()
 
     model = YOLO("yolov8n-pose.pt").to('cuda')
-    # model.export(format="engine")
-    # model = YOLO("yolov8n-pose.engine").to("cuda")
 
     tracker = sv.ByteTrack()
     box_annotator = sv.BoundingBoxAnnotator()
     label_annotator = sv.LabelAnnotator()
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    video_info = sv.VideoInfo.from_video_path(video_path=source_video_path)
+    frame_generator = sv.get_video_frames_generator(source_path=args.source_video_path)
+    video_info = sv.VideoInfo.from_video_path(video_path=args.source_video_path)
 
-    with sv.VideoSink(target_path=target_video_path, video_info=video_info) as sink:
+    with sv.VideoSink(target_path=args.target_video_path, video_info=video_info) as sink:
         for frame in tqdm(frame_generator, total=video_info.total_frames):
 
             # frame = torch.tensor(frame.transpose(2,0,1)).to("cuda").unsqueeze(0)
@@ -68,20 +69,5 @@ if __name__ == "__main__" :
                 if pt1[0] == 0. or pt2[0] == 0. : continue
                 cv2.line(frame, (int(pt1[0]), int(pt1[1])), (int(pt2[0]), int(pt2[1])), (255, 0, 0), 2)  # 青色で線を描画
 
-#            detections = sv.Detections.from_ultralytics(results)
-#            detections = tracker.update_with_detections(detections)
-
-            # slicer = sv.InferenceSlicer(callback=callback)
-            # detections = slicer(image=frame)
-
-#            annotated_frame = box_annotator.annotate(
-#                scene=frame.copy(), detections=detections
-#            )
-#
-#            annotated_labeled_frame = label_annotator.annotate(
-#                scene=annotated_frame, detections=detections
-#            )
-
-#            sink.write_frame(frame=annotated_labeled_frame)
             sink.write_frame(frame=frame)
     
